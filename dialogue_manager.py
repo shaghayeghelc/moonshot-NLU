1833fab9a5fae57e10176bb6edda71387fe4cf22
SHAGHAYEGH SHAHROKHI TEHRANI <SHARON.SHAHROKHI.TEHRANI@cbc.ca>
2019-02-01 22:13:40 -0500

    Initial Commit

---
+++ dialogue_manager.py
@@ -0,0 +1,28 @@
+#!usr/bin/env python3
+
+# Importing Packages
+import os
+from utils import *
+import sys
+import json
+
+class DialogueManager():
+    def __init__(self):
+        # Intent classification - English
+        self.intent_classifier_en = unpickle_file('intentClassifierEN')
+        self.tfidf_vectorizer = unpickle_file('tfidfVectorizer')
+
+    def intentClassifierEN(self, request):
+        # Find the intent of request.
+        preprocessed_request = text_prepare(request) 
+        print(preprocessed_request)
+        features = self.tfidf_vectorizer.transform([preprocessed_request]) 
+        print(features)
+        intent = self.intent_classifier_en.predict(features)[0] 
+        print(intent)
+        return intent
+
+if __name__ == "__main__":
+    request = sys.argv[1]
+    DM = DialogueManager()
+    DM.intentClassifierEN(request)
