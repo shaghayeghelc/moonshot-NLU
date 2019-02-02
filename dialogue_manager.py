96186d19b2740b68aeb86bed6c99c14b5bcf8c1b
SHAGHAYEGH SHAHROKHI TEHRANI <SHARON.SHAHROKHI.TEHRANI@cbc.ca>
2019-02-02 00:04:03 -0500

    Add french intent classifier

--- dialogue_manager.py
+++ dialogue_manager.py
@@ -10,6 +10,7 @@ class DialogueManager():
     def __init__(self):
         # Intent classification - English
         self.intent_classifier_en = unpickle_file('intentClassifierEN')
+        self.intent_classifier_fr = unpickle_file('intentClassifierFR')
         self.tfidf_vectorizer = unpickle_file('tfidfVectorizer')
 
     def intentClassifierEN(self, request):
@@ -19,6 +20,16 @@ class DialogueManager():
         features = self.tfidf_vectorizer.transform([preprocessed_request]) 
         print(features)
         intent = self.intent_classifier_en.predict(features)[0] 
+        print(intent)
+        return intent
+
+    def intentClassifierFR(self, request):
+        # Find the intent of request.
+        preprocessed_request = text_prepare(request) 
+        print(preprocessed_request)
+        features = self.tfidf_vectorizer.transform([preprocessed_request]) 
+        print(features)
+        intent = self.intent_classifier_fr.predict(features)[0] 
         print(intent)
         return intent
 
